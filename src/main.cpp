37815d3651be49f9f2ae8c2600dc896529fa23c4
Nico Lerchl <nico@lerchl.one>
2026-02-08 21:20:44 +0100

    really fix response handling on network error now

--- src/main.cpp
+++ src/main.cpp
@@ -213,29 +213,27 @@ void ptrans_job() {
 
     for (;;) {
         auto result = cli.Get("/timetable");
+        std::string formatted_time =
+            std::format("{0:%F_%T}", std::chrono::system_clock::now());
 
-        if (result && result->status == 200) {
+        if (!result) {
+            std::cerr << std::format("{} - {} Could not fetch timetable",
+                                     formatted_time, result->status)
+                      << std::endl;
+        } else if (result->status == 200) {
             timetable.store(
                 std::make_shared<TimetableDto>(parse_timetable(result->body)),
                 std::memory_order_release);
         } else {
-            std::string formatted_time =
-                std::format("{0:%F_%T}", std::chrono::system_clock::now());
-            try {
-                ErrorDto error = parse_error(result->body);
-                std::cerr << std::format(
-                                 "{} - {} Could not fetch timetable: {}",
-                                 formatted_time, result->status, error.message)
-                          << std::endl;
-            } catch (const json::parse_error &e) {
-                std::cerr << std::format("{} - {} Could not fetch timetable",
-                                         formatted_time, result->status)
-                          << std::endl;
-            }
+            ErrorDto error = parse_error(result->body);
+            std::cerr << std::format("{} - {} Could not fetch timetable: {}",
+                                     formatted_time, result->status,
+                                     error.message)
+                      << std::endl;
         }
-
-        std::this_thread::sleep_for(std::chrono::seconds(30));
     }
+
+    std::this_thread::sleep_for(std::chrono::seconds(30));
 }
 
 std::string real_time_indicator(bool real_time, bool late, bool traffic_jam) {
