0fa188077fc13be2b85fd1b09276517a10d55d0d
Nico Lerchl <nico@lerchl.one>
2026-04-19 10:14:53 +0200

    chore: change to 3 displays

--- src/main.cpp
+++ src/main.cpp
@@ -190,7 +190,7 @@ int main(int argc, char *argv[]) {
         "6", "7", "8", "9", ".", ":", ",", "!", "?", "-", "*", "\""};
     const int SF_CHARSET_SIZE = (int)SF_CHARSET.size();
     const int SF_MS_PER_STEP = 25;
-    const int SF_MATRIX_W = 128;
+    const int SF_MATRIX_W = 192;
     const int SF_MATRIX_H = 64;
 
     // TEXT mode grid (font_large)
